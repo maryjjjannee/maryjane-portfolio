6f14caf86fcccbbd6b34a0cecda5aa9707a28f5b
mj <onwanyaskr@gmail.com>
2023-10-11 13:45:41 +0700

    text

--- src/components/About/AboutCard.js
+++ src/components/About/AboutCard.js
@@ -5,31 +5,31 @@ import { BiRightArrow } from "react-icons/bi";
 function AboutCard() {
   return (
     <div className="aboutc" >
-    <Card className="quote-card-view">
-      <Card.Body>
-        <blockquote className="blockquote mb-0">
-          <p style={{ textAlign: "justify" , color : "white" }}>
-            Hi Everyone, My name is <span className="purple"> Onwanya Sakharet </span>
-             I'm <span className="purple"> 22 years old .</span>
-            <br />I am a student in Burapha University.
-            <br />You can call me <span className="purple">Jane  </span>, My major was Information Technology.
-            <br />
-            <br />
-            Apart from coding, some other activities that I love to do!
-          </p>
-          <ul>
-            <li className="about-activity" style={{ color : "MediumPurple" }}>
-              <BiRightArrow /> Playing Games
-            </li>
-            <li className="about-activity" style={{ color : "MediumPurple" }}>
-              <BiRightArrow /> Travelling
-            </li>
-          </ul>
-         
-        </blockquote>
-      </Card.Body>
-    </Card>
-  </div>
+      <Card className="quote-card-view">
+        <Card.Body>
+          <blockquote className="blockquote mb-0">
+            <p style={{ textAlign: "justify", color: "white" }}>
+              Hi Everyone, My name is <span className="purple">Onwanya Sakharet</span>. I'm <span className="purple">22 years old</span>.
+              <br />I am a student at Burapha University.
+              <br />You can call me <span className="purple">Jane</span>. My major is Information Technology.
+              <br />
+              <br />
+              Apart from coding, some other activities that I love to do!
+            </p>
+
+            <ul>
+              <li className="about-activity" style={{ color: "MediumPurple" }}>
+                <BiRightArrow /> Playing Games
+              </li>
+              <li className="about-activity" style={{ color: "MediumPurple" }}>
+                <BiRightArrow /> Travelling
+              </li>
+            </ul>
+
+          </blockquote>
+        </Card.Body>
+      </Card>
+    </div>
   );
 }
 
