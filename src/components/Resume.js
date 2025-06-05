69d2882a18887b86d5e751177eb6148a7fac3138
Jane Onwanya <123064279+maryjjjannee@users.noreply.github.com>
2025-06-06 02:36:05 +0700

    update Resume component to reference new CV.pdf and change button text to 'Download CV'

--- src/components/Resume.js
+++ src/components/Resume.js
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from "react";
 import { Container, Row } from "react-bootstrap";
 import Button from "react-bootstrap/Button";
 import Particle from "./Particle";
-import pdf from "../Assets/Resume.pdf";
+import pdf from "../Assets/CV.pdf";
 import { AiOutlineDownload } from "react-icons/ai";
 import { Document, Page, pdfjs } from "react-pdf";
 import "react-pdf/dist/esm/Page/AnnotationLayer.css";
@@ -34,7 +34,7 @@ function Resume() {
             style={{ maxWidth: "250px" }}
           >
             <AiOutlineDownload />
-            &nbsp;Download Resume
+            &nbsp;Download CV
           </Button>
         </Row>
       </Container>
